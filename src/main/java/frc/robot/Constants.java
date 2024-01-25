0d9c6eabfd7c3d8527f7406272b497dc501f911a
Cheru Berhanu <cheru@cheru.dev>
2024-01-25 16:26:25 -0500

    move CANCoder CAN IDs to Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,12 +27,23 @@ public final class Constants {
   public static final class SwerveConstants {
     public static final int FrontLeftDriveId = 1;
     public static final int FrontLeftTurnId = 2;
+    public static final int FrontLeftEncoderId = 9;
+
+
     public static final int FrontRightDriveId = 3;
     public static final int FrontRightTurnId = 4;
+    public static final int FrontRightEncoderId = 10;
+
+
     public static final int BackLeftDriveId = 5;
     public static final int BackLeftTurnId = 6;
+    public static final int BackLeftEncoderId = 11;
+
+
     public static final int BackRightDriveId = 7;
     public static final int BackRightTurnId = 8;
+    public static final int BackRightEncoderId = 12;
+
   }
 
   private static final RobotType robot = RobotType.SIMBOT;
--- src/main/java/frc/robot/subsystems/drive/ModuleIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSparkFlex.java
@@ -64,28 +64,28 @@ public class ModuleIOSparkFlex implements ModuleIO {
         // front left
         driveSparkFlex = new CANSparkFlex(FrontLeftDriveId, kBrushless);
         turnSparkFlex = new CANSparkFlex(FrontLeftTurnId, kBrushless);
-        cancoder = new CANcoder(0);
+        cancoder = new CANcoder(FrontLeftEncoderId);
         absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
         break;
       case 1:
         // front right
         driveSparkFlex = new CANSparkFlex(FrontRightDriveId, kBrushless);
         turnSparkFlex = new CANSparkFlex(FrontRightTurnId, kBrushless);
-        cancoder = new CANcoder(1);
+        cancoder = new CANcoder(FrontRightEncoderId);
         absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
         break;
       case 2:
         // back left
         driveSparkFlex = new CANSparkFlex(BackLeftDriveId, kBrushless);
         turnSparkFlex = new CANSparkFlex(BackLeftTurnId, kBrushless);
-        cancoder = new CANcoder(2);
+        cancoder = new CANcoder(BackLeftEncoderId);
         absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
         break;
       case 3:
         // back right
         driveSparkFlex = new CANSparkFlex(BackRightDriveId, kBrushless);
         turnSparkFlex = new CANSparkFlex(BackRightTurnId, kBrushless);
-        cancoder = new CANcoder(3);
+        cancoder = new CANcoder(BackRightEncoderId);
         absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
         break;
       default:
