2125fb93d0d2c4dbbb0df2d8e0d5c252e51d0391
Cheru Berhanu <cheru@cheru.dev>
2024-02-12 12:02:55 -0500

    update CAN IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,26 +25,27 @@ import edu.wpi.first.wpilibj.RobotBase;
  */
 public final class Constants {
   public static final class SwerveConstants {
-    public static final int FrontLeftDriveId = 12;
-    public static final int FrontLeftTurnId = 10;
-    public static final int FrontLeftEncoderId = 11;
+    public static final int FrontLeftDriveId = 1;
+    public static final int FrontLeftTurnId = 2;
+    public static final int FrontLeftEncoderId = 3;
 
-    public static final int FrontRightDriveId = 3;
-    public static final int FrontRightTurnId = 1;
-    public static final int FrontRightEncoderId = 2;
+    public static final int FrontRightDriveId = 4;
+    public static final int FrontRightTurnId = 5;
+    public static final int FrontRightEncoderId = 6;
 
-    public static final int BackLeftDriveId = 9;
-    public static final int BackLeftTurnId = 7;
-    public static final int BackLeftEncoderId = 8;
+    public static final int BackLeftDriveId = 7;
+    public static final int BackLeftTurnId = 8;
+    public static final int BackLeftEncoderId = 9;
 
-    public static final int BackRightDriveId = 6;
-    public static final int BackRightTurnId = 4;
-    public static final int BackRightEncoderId = 5;
+    public static final int BackRightDriveId = 10;
+    public static final int BackRightTurnId = 11;
+    public static final int BackRightEncoderId = 12;
   }
 
-  public static final int PigeonId = 13;
+  public static final int PigeonId = 31;
+  public static final int RevPDHId = 32;
 
-  private static final RobotType robot = RobotType.SIMBOT;
+  private static final RobotType robot = RobotType.MAIN_2024;
 
   public static enum RobotType {
     SIMBOT,
