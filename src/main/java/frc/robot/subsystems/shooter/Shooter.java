72aec72473443dcd17782636fa742216a8e7cac1
Cheru Berhanu <cheru@cheru.dev>
2024-02-12 12:05:15 -0500

    update shooter (still in-progress)

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,7 +1,80 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
+import org.littletonrobotics.junction.Logger;
 
 public class Shooter extends SubsystemBase {
+  private ShooterIO io;
+  private ShooterIOInputsAutoLogged inputs;
+  private PIDController topPID;
+  private PIDController bottomPID;
 
+  private SimpleMotorFeedforward topFF;
+  private SimpleMotorFeedforward bottomFF;
+  private SysIdRoutine flywheelSysId;
+
+  public Shooter(ShooterIO io) {
+    this.io = io;
+
+    switch (Constants.getMode()) {
+      case REAL:
+      case REPLAY:
+        topFF = new SimpleMotorFeedforward(0, 0);
+        bottomFF = new SimpleMotorFeedforward(0, 0);
+        topPID = new PIDController(1, 0, 0);
+        bottomPID = new PIDController(1, 0, 0);
+        break;
+      case SIM:
+        topFF = new SimpleMotorFeedforward(0, 0);
+        bottomFF = new SimpleMotorFeedforward(0, 0);
+        topPID = new PIDController(1, 0, 0);
+        bottomPID = new PIDController(1, 0, 0);
+        break;
+    }
+
+    flywheelSysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                state -> Logger.recordOutput("Shooter/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism(
+                voltageMeasure -> {
+                  io.setTopVoltage(voltageMeasure.in(Volts));
+                  io.setBottomVoltage(voltageMeasure.in(Volts));
+                },
+                null,
+                this));
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
+
+  public void setFlywheelSetpointRPM(double top, double bottom) {
+    io.setTopVoltage(
+        topPID.calculate(
+                Units.radiansPerSecondToRotationsPerMinute(inputs.topVelocityRadPerSec), top)
+            + topFF.calculate(top));
+  }
+
+  /** Returns a command to run a quasistatic test in the specified direction. */
+  public Command flywheelSysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return flywheelSysId.quasistatic(direction);
+  }
+
+  /** Returns a command to run a dynamic test in the specified direction. */
+  public Command flywheelSysIdDynamic(SysIdRoutine.Direction direction) {
+    return flywheelSysId.dynamic(direction);
+  }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -3,52 +3,52 @@ package frc.robot.subsystems.shooter;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Unit;
 
 public class ShooterIOSparkMax implements ShooterIO {
-    private CANSparkMax topSpinMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushless);
-    private CANSparkMax bottomSpinMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushless);
-    private CANSparkMax pivotMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushed);
+  private CANSparkMax topSpinMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushless);
+  private CANSparkMax bottomSpinMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushless);
+  private CANSparkMax pivotMotor = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushed);
 
-    private final RelativeEncoder topEncoder;
-    private final RelativeEncoder bottomEncoder;
-    // private final RelativeEncoder pivotEncoder;
+  private final RelativeEncoder topEncoder;
+  private final RelativeEncoder bottomEncoder;
 
-    public ShooterIOSparkMax() {
-        topEncoder = topSpinMotor.getEncoder();
-        bottomEncoder = bottomSpinMotor.getEncoder();
-        // pivotEncoder = pivotMotor.getEncoder();
-    }
+  // private final RelativeEncoder pivotEncoder;
 
-    @Override
-    public void updateInputs(ShooterIOInputs inputs) {
-        inputs.bottomPositionRad = Units.rotationsToRadians(bottomEncoder.getPosition());
-        inputs.topPositionRad = Units.rotationsToRadians(topEncoder.getPosition());
-        // inputs.pivotPosition = Rotation2d.fromRotations(topEncoder.getPosition());
+  public ShooterIOSparkMax() {
+    topEncoder = topSpinMotor.getEncoder();
+    bottomEncoder = bottomSpinMotor.getEncoder();
+    // pivotEncoder = pivotMotor.getEncoder();
+  }
 
-        inputs.bottomVelocityRadPerSec = Units.rotationsToRadians(bottomEncoder.getVelocity());
-        inputs.topVelocityRadPerSec = Units.rotationsToRadians(topEncoder.getVelocity());
-        // inputs.pivotVelocityRadPerSec = Units.rotationsToRadians(pivotEncoder.getVelocity());
+  @Override
+  public void updateInputs(ShooterIOInputs inputs) {
+    inputs.bottomPositionRad = Units.rotationsToRadians(bottomEncoder.getPosition());
+    inputs.topPositionRad = Units.rotationsToRadians(topEncoder.getPosition());
+    // inputs.pivotPosition = Rotation2d.fromRotations(topEncoder.getPosition());
 
-        inputs.bottomAppliedVolts = bottomSpinMotor.getAppliedOutput() * bottomSpinMotor.getBusVoltage();
-        inputs.topAppliedVolts = topSpinMotor.getAppliedOutput() * topSpinMotor.getBusVoltage();
-        // inputs.pivotAppliedVolts = pivotMotor.getAppliedOutput() * pivotMotor.getBusVoltage();
-    }
+    inputs.bottomVelocityRadPerSec = Units.rotationsToRadians(bottomEncoder.getVelocity());
+    inputs.topVelocityRadPerSec = Units.rotationsToRadians(topEncoder.getVelocity());
+    // inputs.pivotVelocityRadPerSec = Units.rotationsToRadians(pivotEncoder.getVelocity());
 
-/*    @Override
-    public void setPivotVoltage(double volts) {
-        pivotMotor.setVoltage(volts);
-    }*/
+    inputs.bottomAppliedVolts =
+        bottomSpinMotor.getAppliedOutput() * bottomSpinMotor.getBusVoltage();
+    inputs.topAppliedVolts = topSpinMotor.getAppliedOutput() * topSpinMotor.getBusVoltage();
+    // inputs.pivotAppliedVolts = pivotMotor.getAppliedOutput() * pivotMotor.getBusVoltage();
+  }
 
-    @Override
-    public void setTopVoltage(double volts) {
-        topSpinMotor.setVoltage(volts);
-    }
+  /*    @Override
+  public void setPivotVoltage(double volts) {
+      pivotMotor.setVoltage(volts);
+  }*/
 
-    @Override
-    public void setBottomVoltage(double volts) {
-        bottomSpinMotor.setVoltage(volts);
-    }
+  @Override
+  public void setTopVoltage(double volts) {
+    topSpinMotor.setVoltage(volts);
+  }
+
+  @Override
+  public void setBottomVoltage(double volts) {
+    bottomSpinMotor.setVoltage(volts);
+  }
 }
