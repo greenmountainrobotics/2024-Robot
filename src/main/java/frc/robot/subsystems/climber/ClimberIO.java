33b4276e819bca615e50d5a9ed284025a044c802
Cheru Berhanu <cheru@cheru.dev>
2024-02-19 16:12:13 -0500

    Climber boilerplate

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.climber;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ClimberIO {
+  @AutoLog
+  class ClimberIOInputs {}
+
+  default void updateInputs(ClimberIOInputs inputs) {}
+}
---
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.climber;
+
+public class ClimberIOSim implements ClimberIO{
+    public ClimberIOSim() {
+
+    }
+
+    @Override
+    public void updateInputs(ClimberIOInputs inputs) {
+
+    }
+}
