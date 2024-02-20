0d9d10dacc427cdcb87a1399dd3f0dbf7cd0eb01
Cheru Berhanu <cheru@cheru.dev>
2024-02-20 14:01:24 -0500

    run spotless on ClimberIOSim

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -1,12 +1,8 @@
 package frc.robot.subsystems.climber;
 
-public class ClimberIOSim implements ClimberIO{
-    public ClimberIOSim() {
+public class ClimberIOSim implements ClimberIO {
+  public ClimberIOSim() {}
 
-    }
-
-    @Override
-    public void updateInputs(ClimberIOInputs inputs) {
-
-    }
+  @Override
+  public void updateInputs(ClimberIOInputs inputs) {}
 }
