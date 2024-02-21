0870fcc87adfa0efae273082fda8a661a0da8a1b
Cheru Berhanu <cheru@cheru.dev>
2024-02-21 15:44:45 -0500

    implement Intake.extend() and Intake.retract()

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,11 +2,25 @@ package frc.robot.constants;
 
 import static edu.wpi.first.math.util.Units.inchesToMeters;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
 public class IntakeConstants {
 
   public static final double MaxExtension = inchesToMeters(8.524);
+  public static final double TargetExtension = inchesToMeters(7.693);
   public static final double MinExtension = inchesToMeters(2.625);
+  public static final double StartRotatingDownwardsExtension =
+      TargetExtension - inchesToMeters(6);
+
   public static final double ExtensionMetersPerRotation = inchesToMeters(1);
   public static final double PivotHeight = inchesToMeters(8.369);
   public static final double ExtensionStartX = inchesToMeters(10.618500);
+
+  public static final Rotation2d TargetArticulation = Rotation2d.fromDegrees(-77);
+  public static final Rotation2d RetractedArticulation = Rotation2d.fromDegrees(90 + 32.724839);
+  public static final Rotation2d HalfwayArticulation = Rotation2d.fromDegrees(180 + 166.600512);
+
+  public static final double ExtensionTolerance = Units.inchesToMeters(0.5);
+  public static final double ArticulationToleranceRad = 0.02;
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.math.MathUtil.angleModulus;
+import static frc.robot.constants.IntakeConstants.*;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
@@ -7,7 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.constants.DriveConstants;
 import frc.robot.constants.IntakeConstants;
 import frc.robot.constants.TunableConstants;
@@ -21,8 +24,9 @@ public class Intake extends SubsystemBase {
   private final PIDController extensionPID;
   private final PIDController articulationPID;
 
-  private double extensionSetpointMeters = 0.0;
-  private Rotation2d articulationSetpoint = Rotation2d.fromDegrees(90);
+  private double extensionSetpointMeters = MinExtension;
+  private double currentExtensionMeters = 0.0;
+  private Rotation2d articulationSetpoint = RetractedArticulation;
 
   public Intake(IntakeIO io) {
     this.io = io;
@@ -41,6 +45,9 @@ public class Intake extends SubsystemBase {
         articulationPID = new PIDController(5, 0, 0.8);
       }
     }
+
+    extensionPID.setSetpoint(extensionSetpointMeters);
+    articulationPID.setSetpoint(angleModulus(articulationSetpoint.getRadians()));
   }
 
   @Override
@@ -50,12 +57,12 @@ public class Intake extends SubsystemBase {
 
     io.articulationRunVoltage(articulationPID.calculate(inputs.articulationPosition.getRadians()));
 
-    double currentExtensionMeters =
+    currentExtensionMeters =
         IntakeConstants.ExtensionMetersPerRotation
                 * Rotation2d.fromRadians(inputs.leftExtensionPositionRad).getRotations()
             + IntakeConstants.MinExtension;
 
-    var voltage = extensionPID.calculate(currentExtensionMeters - IntakeConstants.MinExtension);
+    var voltage = extensionPID.calculate(currentExtensionMeters);
     io.extensionRunVoltage(voltage, voltage);
 
     Logger.recordOutput(
@@ -76,7 +83,7 @@ public class Intake extends SubsystemBase {
     MechanismLigament2d wrist =
         elevator.append(
             new MechanismLigament2d(
-                "wrist", 0.1, articulation.getDegrees(), 10, new Color8Bit(Color.kGreen)));
+                "wrist", 0.1, articulation.getDegrees(), 5, new Color8Bit(Color.kGreen)));
     return mechanism;
   }
 
@@ -85,14 +92,52 @@ public class Intake extends SubsystemBase {
   }
 
   public void setArticulation(Rotation2d rotation) {
-    articulationPID.setSetpoint(rotation.getRadians());
+    articulationPID.setSetpoint(angleModulus(rotation.getRadians()));
     articulationSetpoint = rotation;
   }
 
-  public void setExtension(double coeff) {
-    extensionSetpointMeters =
-        coeff * (IntakeConstants.MaxExtension - IntakeConstants.MinExtension)
-            + IntakeConstants.MinExtension;
-    extensionPID.setSetpoint(extensionSetpointMeters - IntakeConstants.MinExtension);
+  public void setExtension(double extensionSetpointMeters) {
+    this.extensionSetpointMeters = extensionSetpointMeters;
+    extensionPID.setSetpoint(extensionSetpointMeters);
+  }
+
+  public boolean extensionIsAtSetpoint() {
+    return Math.abs(extensionSetpointMeters - currentExtensionMeters) < ExtensionTolerance;
+  }
+
+  public boolean articulationIsAtSetpoint() {
+    return Math.abs(extensionSetpointMeters - currentExtensionMeters) < ArticulationToleranceRad;
+  }
+
+  public double getExtension() {
+    return currentExtensionMeters;
+  }
+
+  public Rotation2d getArticulation() {
+    return inputs.articulationPosition;
+  }
+
+  public Command extend() {
+    return new InstantCommand(() -> setExtension(TargetExtension), this)
+        .andThen(new InstantCommand(() -> setArticulation(HalfwayArticulation), this))
+        .andThen(
+            new RunCommand(
+                () -> {
+                  if (currentExtensionMeters > StartRotatingDownwardsExtension)
+                    setArticulation(
+                        HalfwayArticulation.plus(
+                            TargetArticulation.minus(HalfwayArticulation)
+                                .times(
+                                    (currentExtensionMeters - StartRotatingDownwardsExtension)
+                                        / (TargetExtension - StartRotatingDownwardsExtension))));
+                },
+                this))
+        .until(() -> articulationIsAtSetpoint() && extensionIsAtSetpoint());
+  }
+
+  public Command retract() {
+    return new InstantCommand(() -> setExtension(MinExtension), this)
+        .andThen(new InstantCommand(() -> setArticulation(RetractedArticulation), this))
+        .andThen(new WaitUntilCommand(() -> articulationIsAtSetpoint() && extensionIsAtSetpoint()));
   }
 }
