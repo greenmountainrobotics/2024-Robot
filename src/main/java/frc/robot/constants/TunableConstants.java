054f052e103dcb89e910eb9f7ab1efcb05911f78
Cheru Berhanu <cheru@cheru.dev>
2024-03-12 09:16:30 -0400

    tune constants for intake/drive

--- src/main/java/frc/robot/constants/TunableConstants.java
+++ src/main/java/frc/robot/constants/TunableConstants.java
@@ -4,10 +4,10 @@ public final class TunableConstants {
   public static final double KpX = 3.0;
   public static final double KpY = 3.0;
   public static final double KpTranslation = 3.0;
-  public static final double KpTheta = 5.0;
+  public static final double KpTheta = 7.0;
 
-  public static final double KpIntakeExtension = 1;
-  public static final double KdIntakeExtension = 1;
+  public static final double KpIntakeExtension = 50;
+  public static final double KdIntakeExtension = 0;
 
   public static final double KpIntakeArticulation = 1;
   public static final double KdIntakeArticulation = 1;
