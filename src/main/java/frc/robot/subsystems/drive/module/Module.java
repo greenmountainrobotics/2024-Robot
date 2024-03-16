dbcb548d129a7d4a766449fbc5bc4bed1afdafaa
Cheru Berhanu <cheru@cheru.dev>
2024-03-16 09:34:10 -0400

    updates @ 2024-03-16T09:34:10-04:00

--- src/main/java/frc/robot/subsystems/drive/module/Module.java
+++ src/main/java/frc/robot/subsystems/drive/module/Module.java
@@ -90,7 +90,8 @@ public class Module {
 
     // On first cycle, reset relative turn encoder
     // Wait until absolute angle is nonzero in case it wasn't initialized yet
-    if ((turnRelativeOffset== null || Timer.getFPGATimestamp() - lastTurnOffsetTime > 1) && inputs.turnAbsolutePosition.getRadians() != 0.0) {
+    if ((turnRelativeOffset == null || Timer.getFPGATimestamp() - lastTurnOffsetTime > 1)
+        && inputs.turnAbsolutePosition.getRadians() != 0.0) {
       turnRelativeOffset = inputs.turnAbsolutePosition.minus(inputs.turnPosition);
       lastTurnOffsetTime = Timer.getFPGATimestamp();
     }
