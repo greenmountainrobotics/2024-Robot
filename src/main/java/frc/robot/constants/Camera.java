baebe49c0743704b13bbb485f8852c38cc56173d
Cheru Berhanu <cheru@cheru.dev>
2024-03-29 21:17:03 -0400

    updates @ 2024-03-29T21:17:03-04:00

--- src/main/java/frc/robot/constants/Camera.java
+++ src/main/java/frc/robot/constants/Camera.java
@@ -15,7 +15,7 @@ public enum Camera {
       720,
       Rotation2d.fromDegrees(75),
       new Transform3d(
-          new Translation3d(inchesToMeters(2.091), inchesToMeters(-0.005), inchesToMeters(6.061)),
+          new Translation3d(inchesToMeters(-2.091), inchesToMeters(-0.005), inchesToMeters(6.061)),
           new Rotation3d(0, degreesToRadians(-25), degreesToRadians(180)))),
   FrontLeftCamera(
       "FrontLeftCam",
