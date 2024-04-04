569170bed181254f9271474d6583bb92c334bd2c
Cheru Berhanu <cheru@cheru.dev>
2024-04-04 12:16:56 -0400

    updates @ 2024-04-04T12:16:56-04:00

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -40,6 +40,7 @@ public class IntakeIOReal implements IntakeIO {
 
     rightExtensionMotor.setInverted(true);
     leftExtensionMotor.setInverted(true);
+    spinMotor.setInverted(false);
 
     articulationMotor.setNeutralMode(NeutralMode.Brake);
 
