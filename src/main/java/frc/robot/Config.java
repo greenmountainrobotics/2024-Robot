99a76ced3df0d3398841fc7529d2dd8d034c38f9
Cheru Berhanu <cheru@cheru.dev>
2024-04-04 10:47:44 -0400

    updates @ 2024-04-04T10:47:44-04:00

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -14,7 +14,7 @@
 package frc.robot;
 
 public final class Config {
-  public static final boolean REPLAY = false;
+  public static final boolean REPLAY = true;
 
   /** simulate cameras in simulation */
   public static final boolean SIMULATE_CAMERAS = false;
